df36e620b4b3af7838227860e03bd05b9965d77b
jmjuan <josem.juangracia@gmail.com>
2023-02-05 14:08:41 +0100

    Commmit Jose Manuel

---
+++ Proyecto_AD1/src/JoseManuel/JoseManuel.java
@@ -0,0 +1,15 @@
+package JoseManuel;
+
+public class JoseManuel {
+
+	public static void main(String[] args) {
+		
+		System.out.println(decirHola());
+
+		}
+		
+		public static String decirHola() {
+			return "Hola soy Jose Manuel";
+		}
+
+	}
