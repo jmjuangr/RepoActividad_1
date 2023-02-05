3f2476437f31a362d671135b622f67c9dba9de3d
jmjuan <josem.juangracia@gmail.com>
2023-02-05 16:51:20 +0100

    3 commit cambios comidas clase Miguel

--- Proyecto_AD1/src/Miguel/Miguel.java
+++ Proyecto_AD1/src/Miguel/Miguel.java
@@ -3,19 +3,15 @@ package Miguel;
 public class Miguel {
 
 	public static void main(String[] args) {
-		
 		System.out.println(decirHola());
 		System.out.println("Deporte, Cine, Juegos de mesa");
 		System.out.println(comidaFavorita());
 	}
 
-
-
-public static String decirHola() {
+	public static String decirHola() {
 	  return "Hola soy Miguel";
-}
-public static String comidaFavorita() {
+	}
+	public static String comidaFavorita() {
 	return "migas con huevo, paellita, frijoles.";
-}
-
+	}
 }
