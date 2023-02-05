fd2b06e9f18a9bda5f1c82430cae564ccacd713e
Manuel_Sanchis <manuelsanchistormo@gmail.com>
2023-02-05 16:20:17 +0100

    Tercer commit en la rama develop de Manuel Sanchis

--- Proyecto_AD1/src/ManuelSanchis/ManuelSanchis.java
+++ Proyecto_AD1/src/ManuelSanchis/ManuelSanchis.java
@@ -20,10 +20,6 @@ public class ManuelSanchis {
 		return "Practicar kendo, la fotogria analógica y jugar a Warhammer 6º edición.";
 		
 	}
-	public static String comidas() {
-		return "pizza con piña, tortilla de patata sin cebolla, helado de menta";
-		
-	}
 	
 	public static String comidaFavorita() {
 		return "Arroz al horno, chucrut y sandwiches de Nocilla.";
