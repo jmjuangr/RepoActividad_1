de894818c9d2e712cef93d7e364d0586374798ed
Scyllis <scylliss@protonmail.ch>
2023-02-05 12:57:50 +0100

    Commit en la rama develop de Miguel

---
+++ Proyecto_AD1/src/Miguel/Miguel.java
@@ -0,0 +1,16 @@
+package Miguel;
+
+public class Miguel {
+
+	public static void main(String[] args) {
+		
+		System.out.println(decirHola());
+	
+	}
+
+
+
+public static String decirHola() {
+	  return "Hola soy Miguel";
+}
+}
