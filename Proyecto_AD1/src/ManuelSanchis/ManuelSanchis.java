c3fda0c87a868fcdfd3f2c364c20f1fb1c5e67d4
Manuel_Sanchis <manuelsanchistormo@gmail.com>
2023-02-05 12:58:02 +0100

    Commit en la rama develop de Manuel Sanchis

---
+++ Proyecto_AD1/src/ManuelSanchis/ManuelSanchis.java
@@ -0,0 +1,15 @@
+package ManuelSanchis;
+
+public class ManuelSanchis {
+
+	public static void main(String[] args) {
+		
+		System.out.println(decirHola());
+
+	}
+	
+	public static String decirHola() {
+		return "Hola soy Manu";
+	}
+
+}
