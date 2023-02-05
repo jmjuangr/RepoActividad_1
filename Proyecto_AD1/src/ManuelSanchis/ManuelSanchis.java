040452d4e1b81f72af1813014dfd5642e8cd0e02
Manuel_Sanchis <manuelsanchistormo@gmail.com>
2023-02-05 15:46:36 +0100

    Segundo Commit en la rama develop de Manuel Sanchis

--- Proyecto_AD1/src/ManuelSanchis/ManuelSanchis.java
+++ Proyecto_AD1/src/ManuelSanchis/ManuelSanchis.java
@@ -5,11 +5,18 @@ public class ManuelSanchis {
 	public static void main(String[] args) {
 		
 		System.out.println(decirHola());
+		
+		System.out.println(hobbies());
 
 	}
 	
 	public static String decirHola() {
 		return "Hola soy Manu";
 	}
+	
+	public static String hobbies() {
+		return "Practicar kendo, la fotogria analógica y jugar a Warhammer 6º edición.";
+	}
+
 
 }
