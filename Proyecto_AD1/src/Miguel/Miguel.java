3722c7edc631026c8e87f3c9d7be05704c1782f5
Scyllis <scylliss@protonmail.ch>
2023-02-05 16:38:53 +0100

    Commit de Jose Miguel añadiendo 3 comidas¨

--- Proyecto_AD1/src/Miguel/Miguel.java
+++ Proyecto_AD1/src/Miguel/Miguel.java
@@ -6,11 +6,15 @@ public class Miguel {
 		
 		System.out.println(decirHola());
 	System.out.println("Deporte, Cine, Juegos de mesa");
+	System.out.println(comidaFavorita());
 	}
 
 
 
 public static String decirHola() {
 	  return "Hola soy Miguel";
+}
+public static String comidaFavorita() {
+    return "Carne, marisco y pescado";
 }
 }
