24d68adc755d96c218303538c41dfce918abf0db
jmjuan <josem.juangracia@gmail.com>
2023-02-05 15:37:36 +0100

    Segundo commit de Jose Manuel en la rama develop del proyecto individual

--- Proyecto_AD1/src/JoseManuel/JoseManuel.java
+++ Proyecto_AD1/src/JoseManuel/JoseManuel.java
@@ -5,11 +5,16 @@ public class JoseManuel {
 	public static void main(String[] args) {
 		
 		System.out.println(decirHola());
+		System.out.println(Hobbies());
 
 		}
 		
 		public static String decirHola() {
 			return "Hola soy Jose Manuel";
+		}
+		
+		public static String Hobbies() {
+				return "Mis hobbies son : Senderismo, cine, videojuegos";
 		}
 
 	}
